1013edc73c19e7ac2750e14250d865579687fc3e
django_wang <Django_Wang@trendmicro.com>
2019-02-13 22:50:44 +0800

    adapte with outer tomcat

--- src/main/java/com/ma/cm/Application.java
+++ src/main/java/com/ma/cm/Application.java
@@ -3,13 +3,21 @@ package com.ma.cm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @SpringBootApplication
 @MapperScan("com.ma.cm.mapper")
-public class Application {
-
+public class Application extends SpringBootServletInitializer {
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+    
+    
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
